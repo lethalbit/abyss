1af25f6c946a37f83f8229a8c5e07cd89f297dcb
Aki Van Ness <aki@lethalbit.net>
2024-08-03 13:44:45 -0700

    cfloat: added primitive tests

---
+++ tests/cfloat.cc
@@ -0,0 +1,164 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+#include <abyss/cfloat>
+
+#if !defined(FLT_RADIX)
+#	error "FLT_RADIX not defined"
+#endif
+
+#if !defined(DECIMAL_DIG)
+#	error "DECIMAL_DIG not defined"
+#endif
+
+#if !defined(FLT_DECIMAL_DIG)
+#	error "FLT_DECIMAL_DIG not defined"
+#endif
+
+#if !defined(DBL_DECIMAL_DIG)
+#	error "DBL_DECIMAL_DIG not defined"
+#endif
+
+#if !defined(LDBL_DECIMAL_DIG)
+#	error "LDBL_DECIMAL_DIG not defined"
+#endif
+
+#if !defined(FLT_MIN)
+#	error "FLT_MIN not defined"
+#endif
+
+#if !defined(DBL_MIN)
+#	error "DBL_MIN not defined"
+#endif
+
+#if !defined(LDBL_MIN)
+#	error "LDBL_MIN not defined"
+#endif
+
+#if !defined(FLT_TRUE_MIN)
+#	error "FLT_TRUE_MIN not defined"
+#endif
+
+#if !defined(DBL_TRUE_MIN)
+#	error "DBL_TRUE_MIN not defined"
+#endif
+
+#if !defined(LDBL_TRUE_MIN)
+#	error "LDBL_TRUE_MIN not defined"
+#endif
+
+#if !defined(FLT_MAX)
+#	error "FLT_MAX not defined"
+#endif
+
+#if !defined(DBL_MAX)
+#	error "DBL_MAX not defined"
+#endif
+
+#if !defined(LDBL_MAX)
+#	error "LDBL_MAX not defined"
+#endif
+
+#if !defined(FLT_EPSILON)
+#	error "FLT_EPSILON not defined"
+#endif
+
+#if !defined(DBL_EPSILON)
+#	error "DBL_EPSILON not defined"
+#endif
+
+#if !defined(LDBL_EPSILON)
+#	error "LDBL_EPSILON not defined"
+#endif
+
+#if !defined(FLT_DIG)
+#	error "FLT_DIG not defined"
+#endif
+
+#if !defined(DBL_DIG)
+#	error "DBL_DIG not defined"
+#endif
+
+#if !defined(LDBL_DIG)
+#	error "LDBL_DIG not defined"
+#endif
+
+#if !defined(FLT_MANT_DIG)
+#	error "FLT_MANT_DIG not defined"
+#endif
+
+#if !defined(DBL_MANT_DIG)
+#	error "DBL_MANT_DIG not defined"
+#endif
+
+#if !defined(LDBL_MANT_DIG)
+#	error "LDBL_MANT_DIG not defined"
+#endif
+
+#if !defined(FLT_MIN_EXP)
+#	error "FLT_MIN_EXP not defined"
+#endif
+
+#if !defined(DBL_MIN_EXP)
+#	error "DBL_MIN_EXP not defined"
+#endif
+
+#if !defined(LDBL_MIN_EXP)
+#	error "LDBL_MIN_EXP not defined"
+#endif
+
+#if !defined(FLT_MIN_10_EXP)
+#	error "FLT_MIN_10_EXP not defined"
+#endif
+
+#if !defined(DBL_MIN_10_EXP)
+#	error "DBL_MIN_10_EXP not defined"
+#endif
+
+#if !defined(LDBL_MIN_10_EXP)
+#	error "LDBL_MIN_10_EXP not defined"
+#endif
+
+#if !defined(FLT_MAX_EXP)
+#	error "FLT_MAX_EXP not defined"
+#endif
+
+#if !defined(DBL_MAX_EXP)
+#	error "DBL_MAX_EXP not defined"
+#endif
+
+#if !defined(LDBL_MAX_EXP)
+#	error "LDBL_MAX_EXP not defined"
+#endif
+
+#if !defined(DBL_MAX_10_EXP)
+#	error "DBL_MAX_10_EXP not defined"
+#endif
+
+#if !defined(LDBL_MAX_10_EXP)
+#	error "LDBL_MAX_10_EXP not defined"
+#endif
+
+#if !defined(FLT_ROUNDS)
+#	error "FLT_ROUNDS not defined"
+#endif
+
+#if !defined(FLT_EVAL_METHOD)
+#	error "FLT_EVAL_METHOD not defined"
+#endif
+
+#if !defined(FLT_HAS_SUBNORM)
+#	error "FLT_HAS_SUBNORM not defined"
+#endif
+
+#if !defined(DBL_HAS_SUBNORM)
+#	error "DBL_HAS_SUBNORM not defined"
+#endif
+
+#if !defined(LDBL_HAS_SUBNORM)
+#	error "LDBL_HAS_SUBNORM not defined"
+#endif
+
+// NOLINTNEXTLINE(readability-named-parameter)
+int main(int, char**) {
+	return 0;
+}
